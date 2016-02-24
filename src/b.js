a321ff726745d29c5bcf1739091bda73bfa2954a
GENGSHUANG <abe.keng@gmail.com>
2016-02-24 11:28:11 +0800

    example

--- src/b.js
+++ src/b.js
@@ -1,3 +1,8 @@
 console.log('bbbbbbbbbbbbbbb----------------hhhgggjjjoooooooo');
-let jpg = require('./other/xxxx.jpg');
+let jpg = require('./images/xxxx.jpg');
+// 非./assets/内的资源，不论存在多少子目录，构建的时候都会进行hash运算，生成新的文件名放到./build/的跟下面(本例中xxxx.jpg因为文件过小，已经被转换为base64使用，所以在./build/下找不到相对应的文件，具体参看下面的例子)
+// 注：相同的图片不管存在多少个，最终也只会生成一个文件（因为hash值一样）
 console.log('jpg : ',jpg);
+
+let png = require('./images/pic2.png');
+console.log('png : ',png);
--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -44,7 +44,7 @@ const clientConfig = {
 		cachedAssets: VERBOSE,
 	},
 
-	entry: path.join(__dirname,'../src/client.js'),
+	entry: path.join(__dirname,'../src/main.js'),
 	output: {
 		path: path.join(__dirname,'../build/'),
 		filename: DEBUG ? '[name].js?[hash]' : '[name].[hash].js',
