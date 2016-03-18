8b3a114ae54e6a0b75bdc095020de7bd3dcbb372
GENGSHUANG <abe.keng@gmail.com>
2016-03-18 15:10:47 +0800

    完美实现外部模块集成

---
+++ template/doc/module-example/index.js
@@ -0,0 +1,7 @@
+import s from './index.scss';
+import gif from './a.gif';
+console.log('The git is : ',gif);
+import data from './data.json';
+console.log('The data in json file is : ',data);
+console.log('The text in text file is : ',require('./book.txt'));
+export const title = 'This is hinowsss';
