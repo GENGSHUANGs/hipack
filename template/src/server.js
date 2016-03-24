e1ed2f2e6ecd838998fb290a4139d8a067134f6d
GENGSHUANG <abe.keng@gmail.com>
2016-03-24 08:43:22 +0800

    初步集成 anyproxy， 把真实的url请求数据代理到本地，使前端开发在数据交互层面独立

--- template/src/server.js
+++ template/src/server.js
@@ -6,16 +6,13 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE.txt file in the root directory of this source tree.
  */
-
+import fs from 'fs';
 import 'babel-core/polyfill';
 import path from 'path';
 import express from 'express';
-// import React from 'react';
-// import ReactDOM from 'react-dom/server';
-// import Router from './routes';
-// import Html from './components/Html';
-// import assets from './assets';
+import colors from 'colors';
 import { port } from './config';
+import proxy from 'anyproxy';
 
 const server = global.server = express();
 
@@ -30,35 +27,50 @@ server.use(express.static(path.join(__dirname, '/')));
 // server.use('/api/content', require('./api/content'));
 
 //
-// Register server-side rendering middleware
-// -----------------------------------------------------------------------------
-// server.get('*', async (req, res, next) => {
-//   try {
-//     let statusCode = 200;
-//     const data = { title: '', description: '', css: '', body: '', entry: assets.main.js };
-//     const css = [];
-//     const context = {
-//       insertCss: styles => css.push(styles._getCss()),
-//       onSetTitle: value => data.title = value,
-//       onSetMeta: (key, value) => data[key] = value,
-//       onPageNotFound: () => statusCode = 404,
-//     };
-
-//     await Router.dispatch({ path: req.path, query: req.query, context }, (state, component) => {
-//       data.body = ReactDOM.renderToString(component);
-//       data.css = css.join('');
-//     });
-//     const html = ReactDOM.renderToStaticMarkup(<Html context={context} {...data} />);
-//     res.status(statusCode).send('<!doctype html>\n' + html);
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
-//
 // Launch the server
 // -----------------------------------------------------------------------------
 server.listen(port, () => {
 	/* eslint-disable no-console */
 	console.log(`The server is running at http://localhost:${port}/`);
+
+	const getLocalResourcePath = (req) => {
+		// console.log('>>:',req.headers.host,req.url,req.method,req.port);
+		const needReplace = req.url.indexOf(':3000/api/') !== -1;
+		if(!needReplace){
+			return false;
+		}
+
+		let uri = req.url.substring(req.url.indexOf(':3000/api/') + 10 );
+		if(uri.indexOf('?') !== -1){
+			uri = uri.substring(0,uri.indexOf('?'));
+		}
+		uri = './api/' + uri.replace(/\//g,'_') + '.json';
+		return uri;
+	};
+
+	const options = {
+	    type          : "http",
+	    port          : 8005,
+	    hostname      : "localhost",
+	    rule          : {
+	    	shouldUseLocalResponse:(req) =>{
+	    		return !!getLocalResourcePath(req);
+	    	},
+	    	dealLocalResponse : function(req,reqBody,callback){
+	    		let localPath = getLocalResourcePath(req);
+	  			localPath = path.join(process.argv[1],'../../src/' + localPath);
+	    		const responseText = JSON.stringify(JSON.parse(fs.readFileSync(localPath)));
+	    		console.log('DEV PROXY FOR : ',colors.green(localPath));
+	    		console.log(responseText + '\n');
+		        callback(200,{"content-type":"text/json"},responseText);
+			},
+		},
+	    dbFile        : null,  // optional, save request data to a specified file, will use in-memory db if not specified
+	    webPort       : 8006,  // optional, port for web interface
+	    socketPort    : 8007,  // optional, internal port for web socket, replace this when it is conflict with your own service
+	    throttle      : 1000000,    // optional, speed limit in kb/s
+	    disableWebInterface : false, //optional, set it when you don't want to use the web interface
+	    silent        : true, //optional, do not print anything into terminal. do not set it when you are still debugging.
+	};
+	new proxy.proxyServer(options);
 });
